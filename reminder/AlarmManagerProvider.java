fc5ebdce35462e248588a840003490814d6bd834
AGABA DAVIS <you@example.com>
2018-08-01 07:10:53 +0300

    add java models for geofrencing and task

---
+++ reminder/AlarmManagerProvider.java
@@ -0,0 +1,25 @@
+package com.example.iryn.reminder;
+
+import android.app.AlarmManager;
+import android.content.Context;
+
+/**
+ * Created by iryn 21/06/2018.
+ */
+
+public class AlarmManagerProvider {
+    private static final String TAG = AlarmManagerProvider.class.getSimpleName();
+    private static AlarmManager sAlarmManager;
+    public static synchronized void injectAlarmManager(AlarmManager alarmManager) {
+        if (sAlarmManager != null) {
+            throw new IllegalStateException("Alarm Manager Already Set");
+        }
+        sAlarmManager = alarmManager;
+    }
+    /*package*/ static synchronized AlarmManager getAlarmManager(Context context) {
+        if (sAlarmManager == null) {
+            sAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        }
+        return sAlarmManager;
+    }
+}
